01a9dd7c112b7aabdb391c238cb8814ddd8ce5b8
Taylor Munnerlyn <taylormunnerlyn@gmail.com>
2016-05-20 10:48:30 -0600

    FizzBuzz commit

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
 // write a program called fizzBuzz that console.logs numbers from 1 to 100, but for multiples of three print 'fizz,' for multiples of five print 'buzz,' and for multiples of both three and five print 'fizzbuzz.' Otherwise, do nothing.
+
+function fizzBuzz() {
+  for (var i = 1; i < 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) console.log("fizzbuzz");
+    else if (i % 3 === 0) console.log("fizz");
+    else if (i % 5 === 0) console.log("buzz");
+    else {
+      console.log(i);
+    }
+  }
+}
